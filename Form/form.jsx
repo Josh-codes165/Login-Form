f4610e6518a3b3c93a153b42377ddd574811e65f
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-01-08 00:45:15 +0100

    Joshua Commit

--- Form/form.jsx
+++ Form/form.jsx
@@ -1,50 +1,48 @@
-import React,{useState } from "react";
+import React, { useState } from "react";
 import "./form.css";
 import mail from "../src/assets/mail.png";
 import padlock from "../src/assets/padlock.png";
 import { Link } from "react-router-dom";
 
 function Form() {
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [userInput, setUserInput] = useState({
-        email: "",
-        password: ""
-    })
+  const handleChange = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = (e) => {
-        setUserInput({...userInput, [e.target.name]: e.target.value})
+  const validate = () => {
+    if (!userInput.email) {
+      alert("Please Enter your Email");
+      return;
+    }
+    if (!userInput.password) {
+      alert("Please Enter your Password");
+      return;
     }
 
-    const validate = () => {
-        if(!userInput.email){
-            alert("Please Enter your Email")
-            return
-        }
-        if(!userInput.password){
-            alert("Please Enter your Password")
-            return
-        }
-
-        if(userInput.password.length < 6){
-            alert("Password must be at least 6 characters long")
-            return
-        }
-        return true;
-
+    if (userInput.password.length < 6) {
+      alert("Password must be at least 6 characters long");
+      return;
     }
+    return true;
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const isValid = validate()
-        if(!isValid) return;
-        console.log(userInput.email)
-        console.log(userInput.password)
-        alert("Welcome Back!")
-        setUserInput({
-        email: " ",
-        password: " "
-    })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+    console.log(userInput.email);
+    console.log(userInput.password);
+    alert("Welcome Back!");
+    setUserInput({
+      email: " ",
+      password: " ",
+    });
+  };
   return (
     <div className="form-container">
       <div className="texts">
@@ -56,20 +54,36 @@ function Form() {
           <label htmlFor="">Email</label>
           <div className="input-container">
             <img src={mail} alt="mail-icon" className="image" />
-            <input type="email" name="email" onChange={handleChange} placeholder="Enter your email" />
+            <input
+              type="email"
+              name="email"
+              value={userInput.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+            />
           </div>
         </div>
         <div className="grouped-input">
           <label htmlFor="">Password</label>
           <div className="input-container">
             <img src={padlock} alt="padlock-icon" className="image" />
-            <input type="password" name="password" onChange={handleChange} placeholder="Enter your password" />
+            <input
+              type="password"
+              name="password"
+              value={userInput.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+            />
           </div>
         </div>
         <div className="grouped-input.small">
           <button type="submit">Login</button>
-          <small className="small">Dont have an account? 
-            <Link to="/form1"><span className="span">Sign up</span></Link></small>
+          <small className="small">
+            Dont have an account?
+            <Link to="/form1">
+              <span className="span">Sign up</span>
+            </Link>
+          </small>
         </div>
       </form>
     </div>
